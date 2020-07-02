6ee02ee0f758d334a9c6daca2ebe2fad087472c6
Jean Louis Borba Rosa Salbego <jlouiss@users.noreply.github.com>
2020-07-02 21:14:47 +0100

    Create readability.c

---
+++ pset2/readability.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(int argc, string argv[])
+{
+    string text = get_string("Text: ");
+    int letterscount = 0;
+    int wordcount = 1;
+    int sentencecount = 0;
+
+    // count words
+    for (int i = 0; i < strlen(text); i++)
+    {
+       if ((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z'))
+       {
+           letterscount++;
+       }
+       else if (text[i] == ' ')
+       {
+           wordcount++;
+       }
+       else if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+       {
+           sentencecount++;
+       }
+    }
+
+    // calculate grade
+    float grade = 0.0588 * (100 * (float) letterscount / (float) wordcount) - 0.296 * (100 * (float) sentencecount / (float) wordcount) - 15.8;
+    if (grade < 16 && grade >= 0)
+    {
+        printf("Grade %i\n", (int) round(grade));
+    }
+    else if (grade >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Before Grade 1\n");
+    }
+}
