065efc2babe7fb9a21a7d9a14e8dae39735a4b52
Jean Louis Borba Rosa Salbego <jlouiss@users.noreply.github.com>
2020-07-10 22:10:12 +0000

    pset2 update

--- pset2/readability.c
+++ pset2/readability.c
@@ -4,29 +4,29 @@
 int main(int argc, string argv[])
 {
     string text = get_string("Text: ");
-    int letterscount = 0;
-    int wordcount = 1;
-    int sentencecount = 0;
+    int letters_count = 0;
+    int word_count = 1;
+    int sentence_count = 0;
 
     // count words
     for (int i = 0; i < strlen(text); i++)
     {
-       if ((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z'))
-       {
-           letterscount++;
-       }
-       else if (text[i] == ' ')
-       {
-           wordcount++;
-       }
-       else if (text[i] == '.' || text[i] == '!' || text[i] == '?')
-       {
-           sentencecount++;
-       }
+        if ((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z'))
+        {
+            letters_count++;
+        }
+        else if (text[i] == ' ')
+        {
+            word_count++;
+        }
+        else if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            sentence_count++;
+        }
     }
 
     // calculate grade
-    float grade = 0.0588 * (100 * (float) letterscount / (float) wordcount) - 0.296 * (100 * (float) sentencecount / (float) wordcount) - 15.8;
+    float grade = 0.0588 * (100 * letters_count / (float) word_count) - 0.296 * (100 * sentence_count / (float) word_count) - 15.8;
     if (grade < 16 && grade >= 0)
     {
         printf("Grade %i\n", (int) round(grade));
